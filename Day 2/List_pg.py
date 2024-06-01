522ac8450864c5668a0d12ba90a2c8f7b71d85e0
Tishadhameliya12345 <159407986+Tishadhameliya12345@users.noreply.github.com>
2024-06-01 13:52:12 +0530

    Python Lists

--- Day 2/List_pg.py
+++ Day 2/List_pg.py
@@ -48,3 +48,55 @@ print(List_1[0][-1])
 #Remove List from List
 List_1.remove("Tisha")
 print(List_1)
+
+# Using a Loop
+
+#For loop
+List1=["Tisha",123456,"T","Harsh",24457644,"H",True]
+for i in List1:
+    print(i)
+    
+# While loop
+while i<=len(List1):
+    print(List1[1])
+    i=i+1
+
+# List Comprehension
+Animal=["Dog","Cat","Donkey","Rabbit","Crab","Fish","Tiger","Lion"]
+New_Animal_List=[]
+
+for i in Animal:
+    if "a" in i:
+        New_Animal_List.append(i)
+        
+print(New_Animal_List)
+
+# Short list
+Animal.sort()
+# if want to in desendingn order then write parameters like this 
+# Animal.sort(reverse=True) or reverse() function
+print(Animal)
+
+# Copy list 
+# In python you can't copy using List1==List2
+# use the built-in List method copy()
+New_List=Animal.copy()
+print(New_List)
+
+#********************************
+# Join two List
+A=["Tisha","Harsh","Jay"]
+B=[1,2,3,4,5]
+# Using + Operetor
+C = A + B
+
+#Using append()
+for x in B:
+    A.append(B)
+
+print(A)
+
+#Using extend()
+A.extend(B)
+
+#*****************************
